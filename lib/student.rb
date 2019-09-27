839caf48d9e3dfc00e956d7b39a3f21054214957
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-27 19:03:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name, grade)
+  def initialize(name = nil, grade = nil)
     @name = name
     @grade = grade
   end 
